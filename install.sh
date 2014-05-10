45a6cc0eae5066b14770404b1c748674a8870337
chiayenhung <hodalalala@gmail.com>
2014-05-09 22:38:58 -0400

    slight change for install script

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@ git clone git://github.com/ry/node.git
 cd node
 ./configure
 make
-sudo make installsudo npm install -g grunt-cli
+sudo make install
+sudo npm install -g grunt-cli
+npm install
 grunt
